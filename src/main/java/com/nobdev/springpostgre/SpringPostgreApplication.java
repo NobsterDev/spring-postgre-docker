c5396a904d4bafac5f98a459c456b33d8dc2059f
Nobster <furkanka10@gmail.com>
2022-06-12 02:49:50 +0300

    init

---
+++ src/main/java/com/nobdev/springpostgre/SpringPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.nobdev.springpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgreApplication.class, args);
+	}
+
+}
